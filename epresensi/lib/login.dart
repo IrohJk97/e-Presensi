63702015a96cff5d825e65b64ae0e096c44f7ecf
sulaksana234 <sulaksana60@gmail.com>
2024-05-11 18:59:06 +0800

    update

--- epresensi/lib/login.dart
+++ epresensi/lib/login.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,9 +14,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
   bool rememberMe = false;
   bool _isPasswordVisible = false;
-  
+  bool _isLoading = false;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -50,7 +56,6 @@ class _LoginPageState extends State<LoginPage> {
                 SizedBox(
                   height: size.height * 0.02,
                 ),
-               
               ],
             ),
           ),
@@ -59,95 +64,54 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-Widget logo(double height_, double width_) {
-  return Image.asset(
-    'assets/images/stepThree.png',
-    height: 200.0,
-    width: 200.0,
-    // You can add more properties here, such as fit, alignment, etc.
-  );
-}
+  Widget logo(double height_, double width_) {
+    return Image.asset(
+      'assets/images/stepThree.png',
+      height: 200.0,
+      width: 200.0,
+    );
+  }
 
-Widget richText(double fontSize) {
-  return Padding(
-    padding: const EdgeInsets.all(10.0), // Add padding around the rich text
-    child: Text.rich(
-      TextSpan(
-        style: GoogleFonts.inter(
-          fontSize: fontSize,
-          color: const Color(0xFF21899C),
-          letterSpacing: 3,
-          height: 1.03,
-        ),
-        children: [
-          TextSpan(
-            text: 'LOGIN\n\n',
-            style: TextStyle(
-              fontWeight: FontWeight.bold, // Make "LOGIN" bold
-              color: Colors.black, // Set the color to black
-            ),
+  Widget richText(double fontSize) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text.rich(
+        TextSpan(
+          style: GoogleFonts.inter(
+            fontSize: fontSize,
+            color: const Color(0xFF21899C),
+            letterSpacing: 3,
+            height: 1.03,
           ),
-          TextSpan(
-            text: 'Please sign in to continue',
-            style: TextStyle(
-              color: Colors.black,
+          children: [
+            TextSpan(
+              text: 'LOGIN\n\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
-          ),
-        ],
-      ),
-      textAlign: TextAlign.left,
-    ),
-  );
-}
-
-
-
-
-Widget usernameTextField(Size size) {
-  return Container(
-    alignment: Alignment.center,
-    height: size.height / 14, // Reduced height
-    padding: const EdgeInsets.symmetric(horizontal: 16),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(8.0),
-      border: Border.all(
-        width: 1.0,
-        color: const Color(0xFFEFEFEF),
-      ),
-    ),
-    child: TextField(
-      style: GoogleFonts.inter(
-        fontSize: 16.0,
-        color: const Color(0xFF15224F),
-      ),
-      maxLines: 1,
-      cursorColor: const Color(0xFF15224F),
-      decoration: InputDecoration(
-        labelText: 'Username', // Change the label to 'Username'
-        labelStyle: GoogleFonts.inter(
-          fontSize: 12.0,
-          color: const Color(0xFF969AA8),
+            TextSpan(
+              text: 'Please sign in to continue',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ],
         ),
-        border: InputBorder.none,
-        contentPadding: EdgeInsets.zero,
-        prefixIcon: Icon(
-          Icons.person, // Use Icons.person for a user icon
-          color: const Color(0xFF969AA8), // Set the icon color
-        ),
+        textAlign: TextAlign.left,
       ),
-    ),
-  );
-}
+    );
+  }
 
-
-
-
- Widget passwordTextField(Size size) {
+  Widget usernameTextField(Size size) {
     return Container(
       alignment: Alignment.center,
-      height: size.height / 14,
+      height: size.height / 17,
       padding: const EdgeInsets.symmetric(horizontal: 16),
+      width: size.width * 0.85,
       decoration: BoxDecoration(
+        color: const Color(0xFF21899C),
         borderRadius: BorderRadius.circular(8.0),
         border: Border.all(
           width: 1.0,
@@ -155,105 +119,152 @@ Widget usernameTextField(Size size) {
         ),
       ),
       child: TextField(
+        controller: _usernameController,
+        style: GoogleFonts.inter(
+          fontSize: 16.0,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        maxLines: 1,
+        cursorColor: const Color(0xFF15224F),
+        decoration: InputDecoration(
+          labelText: 'Username',
+          labelStyle: GoogleFonts.inter(
+            fontSize: 12.0,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.zero,
+          prefixIcon: Icon(
+            Icons.person,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget passwordTextField(Size size) {
+    return Container(
+      alignment: Alignment.center,
+      height: size.height / 17,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      width: size.width * 0.85,
+      decoration: BoxDecoration(
+        color: const Color(0xFF21899C),
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(
+          width: 1.0,
+          color: const Color(0xFFEFEFEF),
+        ),
+      ),
+      child: TextField(
+        controller: _passwordController,
         style: GoogleFonts.inter(
           fontSize: 16.0,
           color: const Color(0xFF15224F),
         ),
         maxLines: 1,
-        obscureText: !_isPasswordVisible, // Toggle obscureText based on _isPasswordVisible
+        obscureText: !_isPasswordVisible,
         keyboardType: TextInputType.visiblePassword,
         cursorColor: const Color(0xFF15224F),
         decoration: InputDecoration(
           labelText: 'Password',
           labelStyle: GoogleFonts.inter(
             fontSize: 12.0,
-            color: const Color(0xFF969AA8),
+            color: Color.fromARGB(255, 255, 255, 255),
           ),
           border: InputBorder.none,
           contentPadding: EdgeInsets.zero,
           prefixIcon: Icon(
             Icons.lock,
-            color: const Color(0xFF969AA8),
+            color: Color.fromARGB(255, 255, 255, 255),
           ),
           suffixIcon: IconButton(
             icon: Icon(
               _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-              color: const Color(0xFF969AA8),
+              color: Color.fromARGB(255, 255, 255, 255),
             ),
             onPressed: () {
               setState(() {
-                _isPasswordVisible = !_isPasswordVisible; // Toggle password visibility
+                _isPasswordVisible = !_isPasswordVisible;
               });
             },
           ),
         ),
       ),
     );
- }
-
-
+  }
 
   Widget rememberMeCheckbox() {
-    return CheckboxListTile(
-      contentPadding: EdgeInsets.zero,
-      title: Text(
-        'Remember me',
-        style: GoogleFonts.inter(
-          fontSize: 16.0,
-          color: const Color(0xFF15224F),
+    return Center(
+      child: SizedBox(
+        width: 200,
+        child: CheckboxListTile(
+          contentPadding: EdgeInsets.zero,
+          title: Text(
+            'Remember me',
+            style: GoogleFonts.inter(
+              fontSize: 16.0,
+              color: const Color(0xFF15224F),
+            ),
+          ),
+          value: rememberMe,
+          onChanged: (newValue) {
+            setState(() {
+              rememberMe = newValue!;
+            });
+          },
+          controlAffinity: ListTileControlAffinity.leading,
+          activeColor: const Color(0xFF21899C),
         ),
       ),
-      value: rememberMe,
-      onChanged: (newValue) {
-        setState(() {
-          rememberMe = newValue!;
-        });
-      },
-      controlAffinity: ListTileControlAffinity.leading,
-      activeColor: const Color(0xFF21899C),
     );
   }
 
-Widget signInButton(Size size) {
-  return Container(
-    alignment: Alignment.center,
-    height: size.height / 14, // Reduced height
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(25.0), // Reduced border radius
-      color: const Color(0xFF21899C),
-      boxShadow: [
-        BoxShadow(
-          color: const Color(0xFF4C2E84).withOpacity(0.2),
-          offset: const Offset(0, 10.0), // Reduced offset
-          blurRadius: 30.0, // Reduced blur radius
+
+
+  Widget signInButton(Size size) {
+    return GestureDetector(
+      onTap: _signIn, // Call _signIn function
+      child: Container(
+        alignment: Alignment.center,
+        height: size.height / 17,
+        width: size.width * 0.85,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25.0),
+          color: const Color(0xFF21899C),
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0xFF4C2E84).withOpacity(0.2),
+              offset: const Offset(0, 10.0),
+              blurRadius: 30.0,
+            ),
+          ],
         ),
-      ],
-    ),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          Icons.login, // Add your desired icon
-          color: Colors.white, // Set icon color
-          size: 20, // Set icon size
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.login,
+              color: Colors.white,
+              size: 20,
+            ),
+            SizedBox(width: 10),
+            Text(
+              'Sign in',
+              style: GoogleFonts.inter(
+                fontSize: 14.0,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+                height: 1.5,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
-        SizedBox(width: 10), // Add some space between icon and text
-        Text(
-          'Sign in',
-          style: GoogleFonts.inter(
-            fontSize: 14.0, // Reduced font size
-            color: Colors.white,
-            fontWeight: FontWeight.w600,
-            height: 1.5,
-          ),
-          textAlign: TextAlign.center,
-        ),
-      ],
-    ),
-  );
+        
+      ),
+    );
+  }
 }
-
-}
-  
-
-
