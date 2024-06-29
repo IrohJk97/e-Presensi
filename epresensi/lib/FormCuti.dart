274c1d814a7c25fcf9d9e65bf318d4b5b6b8944c
sulaksana234 <sulaksana60@gmail.com>
2024-06-29 14:38:34 +0800

    update

--- epresensi/lib/FormCuti.dart
+++ epresensi/lib/FormCuti.dart
@@ -1,5 +1,7 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:image_input/image_input.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const FormCuti());
@@ -26,16 +28,19 @@ class Cuti extends StatefulWidget {
 
 class _CutiPageState extends State<Cuti> {
   final List<String> items = [
-    'Item1',
-    'Item2',
-    'Item3',
-    'Item4',
-    'Item5',
-    'Item6',
-    'Item7',
-    'Item8',
+    'Jenis Cuti',
+    'Cuti Tahunan',
+    'Cuti Sakit',
+    'Cuti Bersalin',
+    'Cuti Ibadah Haji',
+    'Cuti Umroh',
+    'Cuti Kewafatan Keluarga',
+    'Cuti Tugas Belajar',
   ];
   String? selectedValue;
+  DateTime? _selectedDate;
+  XFile? profileAvatarCurrentImage;
+  bool allowEdit = true;
 
   @override
   Widget build(BuildContext context) {
@@ -47,52 +52,19 @@ class _CutiPageState extends State<Cuti> {
             Container(
               width: 357,
               height: 33,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    top: 0,
-                    child: Container(
-                      width: 42,
-                      height: 33,
-                      clipBehavior: Clip.antiAlias,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 32,
-                    top: 0,
-                    child: SizedBox(
-                      width: 288,
-                      height: 32,
-                      child: Text(
-                        'Formulir Pengajuan Cuti',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w600,
-                          height: 0,
-                          letterSpacing: -0.33,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+              child: Text(
+                'Formulir Pengajuan Cuti',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
+                  letterSpacing: -0.33,
+                ),
               ),
             ),
-            SizedBox(
-              height: 0,
-            ),
+            SizedBox(height: 8),
             Text(
               'Mohon untuk mengisi formulir yang telah disediakan, Terimakasih',
               style: TextStyle(
@@ -101,12 +73,9 @@ class _CutiPageState extends State<Cuti> {
                 fontFamily: 'Poppins',
                 fontWeight: FontWeight.w500,
                 height: 0,
-                letterSpacing: -0.33,
               ),
             ),
-            SizedBox(
-              height: 16,
-            ),
+            SizedBox(height: 16),
             DropdownButtonHideUnderline(
               child: DropdownButton2<String>(
                 isExpanded: true,
@@ -115,18 +84,16 @@ class _CutiPageState extends State<Cuti> {
                     Icon(
                       Icons.list,
                       size: 16,
-                      color: Colors.yellow,
+                      color: Colors.blue,
                     ),
-                    SizedBox(
-                      width: 4,
-                    ),
+                    SizedBox(width: 4),
                     Expanded(
                       child: Text(
-                        'Select Item',
+                        'Jenis Cuti',
                         style: TextStyle(
                           fontSize: 14,
                           fontWeight: FontWeight.bold,
-                          color: Colors.yellow,
+                          color: Colors.blue,
                         ),
                         overflow: TextOverflow.ellipsis,
                       ),
@@ -134,19 +101,19 @@ class _CutiPageState extends State<Cuti> {
                   ],
                 ),
                 items: items
-                   .map((String item) => DropdownMenuItem<String>(
+                    .map((String item) => DropdownMenuItem<String>(
                           value: item,
                           child: Text(
                             item,
                             style: TextStyle(
                               fontSize: 14,
                               fontWeight: FontWeight.bold,
-                              color: Colors.white,
+                              color: Colors.blue,
                             ),
                             overflow: TextOverflow.ellipsis,
                           ),
                         ))
-                   .toList(),
+                    .toList(),
                 value: selectedValue,
                 onChanged: (String? value) {
                   setState(() {
@@ -155,33 +122,31 @@ class _CutiPageState extends State<Cuti> {
                 },
                 buttonStyleData: ButtonStyleData(
                   height: 50,
-                  width: 160,
+                  width: 400,
                   padding: const EdgeInsets.only(left: 14, right: 14),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(14),
                     border: Border.all(
-                      color: Colors.black26,
+                      color: Colors.blue,
                     ),
-                    color: Colors.redAccent,
+                    color: Colors.white,
                   ),
                   elevation: 2,
                 ),
                 iconStyleData: IconStyleData(
-                  icon: Icon(
-                    Icons.arrow_forward_ios_outlined,
-                  ),
+                  icon: Icon(Icons.arrow_forward_ios_outlined),
                   iconSize: 14,
-                  iconEnabledColor: Colors.yellow,
+                  iconEnabledColor: Colors.blue,
                   iconDisabledColor: Colors.grey,
                 ),
                 dropdownStyleData: DropdownStyleData(
                   maxHeight: 200,
-                  width: 200,
+                  width: 400,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(14),
-                    color: Colors.redAccent,
+                    color: Colors.white,
                   ),
-                  offset: Offset(-20, 0),
+                  offset: Offset(0, -10),
                   scrollbarTheme: ScrollbarThemeData(
                     radius: Radius.circular(40),
                     thickness: MaterialStateProperty.all<double>(6),
@@ -194,6 +159,150 @@ class _CutiPageState extends State<Cuti> {
                 ),
               ),
             ),
+            SizedBox(height: 16),
+            TextField(
+              style: TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                labelText: 'Tanggal Pengajuan Cuti',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 1,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 1,
+                  ),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.calendar_today),
+                  onPressed: () async {
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2030),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        _selectedDate = pickedDate;
+                      });
+                    }
+                  },
+                ),
+              ),
+              readOnly: true,
+              controller: TextEditingController(
+                text: _selectedDate != null
+                    ? '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
+                    : '',
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              style: TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                labelText: 'Tanggal Kembali Bekerja',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 1,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 1,
+                  ),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.calendar_today),
+                  onPressed: () async {
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2030),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        _selectedDate = pickedDate;
+                      });
+                    }
+                  },
+                ),
+              ),
+              readOnly: true,
+              controller: TextEditingController(
+                text: _selectedDate != null
+                    ? '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
+                    : '',
+              ),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Keperluan',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 1,
+                  ),
+                ),
+              ),
+              maxLines: 5,
+            ),
+            SizedBox(height: 16),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Container(
+                  width: 500,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue[50], // Light blue background color
+                    border: Border.all(
+                      color: Colors.blue, // Blue border color
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(10), // Rounded corners
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.upload_rounded,
+                          color: Colors.blue,
+                          size: 50,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Lampirkan bukti cuti',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        
           ],
         ),
       ),
